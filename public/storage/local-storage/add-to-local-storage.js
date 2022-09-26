1da247d6b412dced42a5d84c4a58cf036cc4ed60
Rene Ajam Rosado <ajrosado@Renes-MacBook-Pro.local>
2022-09-26 00:51:03 -0400

    [storage] add add-to-local-storage.js

--- public/storage/local-storage/add-to-local-storage.js
+++ public/storage/local-storage/add-to-local-storage.js
@@ -1,11 +1,18 @@
+//
+const form = document.querySelector(".add-to-library");
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const data = {};
+    ['title', 'author', 'genre'].forEach((name) => {
+        const property = form.elements[name];
+        data[name] = property.value;
+        property.value = '';
+    });
+    window.localStorage.setItem(data['title'], JSON.stringify(data));
+});
 
 
-
-const form = document.querySelector('.add-to-library');
-const asdf = document.getElementsByClassName('add-to-library');
-
-console.log(form);
-console.log(asdf);
 
 
 
