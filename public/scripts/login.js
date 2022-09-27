8015c19efb616512b40a6401205c6098c191f7de
Rene Ajam Rosado <ajrosado@Renes-MacBook-Pro.local>
2022-09-27 02:12:37 -0400

    [scripts] add logic for session storage, display to DOM

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,30 +1,38 @@
 //
+const session = window.sessionStorage;
+showActiveAccount();
 const mySQLLoginForm = document.querySelector('.mySQL-login-container form');
 
 mySQLLoginForm.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+        const response = JSON.parse(xhr.responseText);
+
+        if (response.username) {
+            session.setItem('active_account', response.username);
+            mySQLLoginForm.reset();
+            showActiveAccount();
+        }
+        else {
+            console.log('error: ' + response.error);
+        }
+    });
 
     let credentials = {};
     ['username', 'password'].forEach((el, index, arr) => {
         credentials[el] = mySQLLoginForm.elements[el].value;
     });
 
-    console.log(3333333333);
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', (event) => {
-        console.log(22222);
-
-        console.log(event);
-
-        console.log(xhr.responseText);
-
-    });
-
-    // xhr.onreadystatechange = () => {
-    //     console.log(xhr.responseText);
-    // };
-
     const url = convertToURL('/mySQL-login', credentials);
     xhr.open('GET', url);
     xhr.send();
 });
+
+// HELPERS
+function showActiveAccount() {
+    if (!session.getItem('active_account')) return;
+    const accountNode = document.querySelector('.login-name');
+    accountNode.innerHTML = `Account: ${session.getItem('active_account')}`;
+}
