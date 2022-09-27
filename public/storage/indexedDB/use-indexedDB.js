e563d7c69fe5c7e73041869557d8771675d00999
Rene Ajam Rosado <ajrosado@Renes-MacBook-Pro.local>
2022-09-27 16:31:32 -0400

    [storage] edit use-indexedDB

--- public/storage/indexedDB/use-indexedDB.js
+++ public/storage/indexedDB/use-indexedDB.js
@@ -11,45 +11,54 @@ observer.observe(document.querySelector('.login-name'), {
 function callback() {
     if (session.getItem('database') !== 'mySQL') return;
 
-    const xhr = new XMLHttpRequest();
-    const parameters = { username: session.getItem('active_account')};
-    const url = convertToURL('/mySQL-load-library', parameters);
-    xhr.open('GET', url);
-    xhr.send();
+    const deleteRequest = indexedDB.deleteDatabase('store');
 
-    xhr.addEventListener('load', () => {
-        let openRequest = indexedDB.open('store', Date.now());
+    deleteRequest.onsuccess = loadIndexedDB;
+    loadIndexedDB();
 
-        openRequest.onerror = (err) => {
-            console.log(err);
-        };
+    function loadIndexedDB() {
+        console.log(1111);
+        const xhr = new XMLHttpRequest();
+        const parameters = { username: session.getItem('active_account')};
+        const url = convertToURL('/mySQL-load-library', parameters);
+        xhr.open('GET', url);
+        xhr.send();
 
-        openRequest.onsuccess = () => {
-            const db = openRequest.result;
-            let transaction = db.transaction('books', 'readwrite');
-            let books = transaction.objectStore('books');
+        xhr.addEventListener('load', () => {
+            let openRequest = indexedDB.open('store', 1);
 
-            const data = JSON.parse(xhr.responseText);
-            data.forEach((item, index, el) => {
-                console.log(item);
-                const book = {
-                    title: item.Title,
-                    author: item.Author,
-                    genre: item.Genre,
+            openRequest.onerror = (err) => {
+                console.log(err);
+            };
+
+            openRequest.onsuccess = () => {
+                console.log(4444);
+                const db = openRequest.result;
+                let transaction = db.transaction('books', 'readwrite');
+                let books = transaction.objectStore('books');
+
+                const data = JSON.parse(xhr.responseText);
+                data.forEach((item, index, el) => {
+                    console.log(item);
+                    const book = {
+                        title: item.Title,
+                        author: item.Author,
+                        genre: item.Genre,
+                    }
+                    books.add(book);
+                });
+                db.close();
+            };
+            
+            openRequest.onupgradeneeded = () => {
+                let db = openRequest.result;
+                if (db.objectStoreNames.contains('books')) {
+                    db.deleteObjectStore('books');
                 }
-                books.add(book);
-            });
-            db.close();
-        };
-        
-        openRequest.onupgradeneeded = () => {
-            let db = openRequest.result;
-            if (db.objectStoreNames.contains('books')) {
-                db.deleteObjectStore('books');
-            }
-            db.createObjectStore('books', {
-                keyPath: 'title',
-            });
-        };
-    });
+                db.createObjectStore('books', {
+                    keyPath: 'title',
+                });
+            };
+        });
+    }
 }
